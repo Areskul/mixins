e2d8b43323e627ae4905c223295d797335c73662
Areskul <fullbringer2.0@gmail.com>
2020-09-06 16:38:06 +0200

    metaTheme feature

--- theme.js
+++ theme.js
@@ -1,14 +1,26 @@
-const theme = {
+export const theme = {
   data: () => ({}),
   computed: {
     isDark: {
-      set: function (bool) {
+      set: function(bool) {
         this.$store.dispatch("theme/setDark", bool);
       },
-      get: function () {
+      get: function() {
         return this.$store.state.theme.isDark;
       },
     },
   },
 };
-export default theme;
+export const metaTheme = {
+  metaInfo() {
+    let fg = "";
+    const cond = this.isDark | !this.isDark;
+    if (cond) {
+      fg = window
+        .getComputedStyle(this.$root.$el)
+        .getPropertyValue("--secondary");
+      console.log(fg);
+      return { meta: [{ name: "theme-color", content: fg }] };
+    }
+  },
+};

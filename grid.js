ef844a9ac44d4ce04cc86faac55db6f7b7e9b3df
Areskul <the.only.areskul@gmail.com>
2020-09-18 15:22:55 +0200

    grid update

--- grid.js
+++ grid.js
@@ -1,58 +1,66 @@
 // export const position = {
-//   props: {
-//     left: Boolean,
-//     right: Boolean,
-//     center: Boolean,
-//   },
-//   mounted() {
-//     this.setPosition();
-//   },
-//   methods: {
-//     setPosition: function() {
-//       const origin = this.$parent.$el.getBoundingClientRect();
-//       const offsetX = 10;
-//       const offsetY = 10;
-//       const x = origin.x + offsetX;
-//       const y = origin.y + offsetY;
-//       this.$el.style.left = x + "px";
-//       this.$el.style.top = y + "px";
-//       console.log(x, y);
-//     },
-//   },
-// };
 export const breakpoints = {
   data: () => ({
-    xs: false,
-    sm: false,
-    md: false,
-    lg: false,
+    xsOnly: false,
+    smOnly: false,
+    mdOnly: false,
+    lgOnly: false,
+    xlOnly: false,
   }),
   beforeDestroy() {
     if (typeof window !== "undefined") {
-      window.removeEventListener("resize", this.onResize, { passive: true });
+      window.removeEventListener("resize", this.onResize, {
+        passive: true,
+      });
     }
   },
   mounted() {
     this.onResize();
     window.addEventListener("resize", this.onResize, { passive: true });
   },
+  computed: {
+    xl: function() {
+      return this.xlOnly ? true : false;
+    },
+    lg: function() {
+      return this.xlOnly || this.lgOnly ? true : false;
+    },
+    md: function() {
+      return this.xlOnly || this.lgOnly || this.mdOnly ? true : false;
+    },
+    sm: function() {
+      return this.xlOnly || this.lgOnly || this.mdOnly || this.smOnly
+        ? true
+        : false;
+    },
+    xs: function() {
+      return this.xlOnly ||
+        this.lgOnly ||
+        this.mdOnly ||
+        this.smOnly ||
+        this.xsOnly
+        ? true
+        : false;
+    },
+  },
   methods: {
     onResize() {
       const width = document.documentElement.clientWidth;
-      this.xs =
+      this.xsOnly =
         width >= this.breakpointValue("xs") &&
-        width >= this.breakpointValue("xl");
-      this.sm =
-        width >= this.breakpointValue("sm") &&
-        width <= this.breakpointValue("xl");
+        width < this.breakpointValue("sm");
 
-      this.md =
+      this.smOnly =
+        width >= this.breakpointValue("sm") &&
+        width <= this.breakpointValue("md");
+
+      this.mdOnly =
         width >= this.breakpointValue("md") &&
-        width <= this.breakpointValue("xl");
-      this.lg =
+        width < this.breakpointValue("lg");
+      this.lgOnly =
         width >= this.breakpointValue("lg") &&
-        width <= this.breakpointValue("xl");
-      this.xl = width >= this.breakpointValue("xl");
+        width < this.breakpointValue("xl");
+      this.xlOnly = width >= this.breakpointValue("xl");
     },
     breakpointValue(name) {
       let value = window

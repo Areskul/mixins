f50b9373fc5dda4fd3f16a660ffa83f668abd61f
Areskul <the.only.areskul@gmail.com>
2020-09-28 17:28:03 +0200

    deprecated

--- style.js
+++ style.js
@@ -1,30 +1,3 @@
-export const border = {
-  props: {
-    border: String,
-    rounded: Boolean,
-  },
-  mounted() {
-    this.setBorderRadius();
-    this.setBorderWidth();
-  },
-  methods: {
-    setBorderRadius: function() {
-      if (this.rounded) {
-        const radius = `var(--border-radius)`;
-        this.$el.style.borderRadius = radius;
-      } else {
-        return;
-      }
-    },
-    setBorderWidth: function() {
-      if (this.border) {
-        this.$el.style.borderWidth = `var(--border-width)`;
-        this.$el.style.borderStyle = "solid";
-        this.$el.style.borderColor = `var(--${this.border})`;
-      }
-    },
-  },
-};
 export const dimension = {
   props: {
     height: String,
